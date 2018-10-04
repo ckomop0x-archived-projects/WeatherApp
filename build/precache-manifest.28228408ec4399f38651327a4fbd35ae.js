8f1406701e9f53affdb5f5c434890b4cf981a26c
Pavel Klochkov <paul.klochkov@gmail.com>
2018-10-04 23:08:18 +0200

    Big update 7

---
+++ build/precache-manifest.28228408ec4399f38651327a4fbd35ae.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "a65dfe9f73dd55aa5b62",
+    "url": "/WeatherApp/static/js/runtime~main.a65dfe9f.js"
+  },
+  {
+    "revision": "a5f9e2dabecac6035258",
+    "url": "/WeatherApp/static/js/main.a5f9e2da.chunk.js"
+  },
+  {
+    "revision": "3768d0c22d134f4c95bf",
+    "url": "/WeatherApp/static/js/1.3768d0c2.chunk.js"
+  },
+  {
+    "revision": "5b96df0264651d4dc7f8204c274e5b74",
+    "url": "/WeatherApp/index.html"
+  }
+];
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.eb1e7b7f0e25e16ad3c15c0ad8d78858.js"
+  "/WeatherApp/precache-manifest.28228408ec4399f38651327a4fbd35ae.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/WeatherApp/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
