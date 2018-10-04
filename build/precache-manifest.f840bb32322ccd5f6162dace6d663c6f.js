0af963647d36a7024935cd6093f21383f86ea3bf
Pavel Klochkov <paul.klochkov@gmail.com>
2018-10-04 02:22:33 +0200

    Big update

---
+++ build/precache-manifest.f840bb32322ccd5f6162dace6d663c6f.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "f1a72cb2fab717151c21",
+    "url": "/static/js/main.f1a72cb2.chunk.js"
+  },
+  {
+    "revision": "88999ab52cfd45ac1aac",
+    "url": "/static/js/1.88999ab5.chunk.js"
+  },
+  {
+    "revision": "f534ac1e4fd855d17b7a6c76da00b705",
+    "url": "/index.html"
+  }
+];
