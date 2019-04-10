f1c77126f52d2328df9a73a039524d3e241915ac
ckomop0x <ckomop0x@users.noreply.github.com>
2019-04-10 22:57:10 +0200

    Update AlertMessage.jsx

--- src/components/AlertMessage.jsx
+++ src/components/AlertMessage.jsx
@@ -1,8 +1,5 @@
 import React from 'react';
 
-const AlertMessage = () =>  {
-    return <div>*Because of http source at api.openweathermap.org you could allow to load
-        resources from unsecure places to let the WeatherApp work.</div>;
-};
+const AlertMessage = () =>  <div>*Because of http source at api.openweathermap.org you could allow to load resources from unsecure places to let the WeatherApp work.</div>
 
 export default AlertMessage;
