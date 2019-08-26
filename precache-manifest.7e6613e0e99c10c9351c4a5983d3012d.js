74338a9880e9e8406c63fe3bba2f1123e9c49ecc
ckomop0x <ckomop0x@users.noreply.github.com>
2019-08-26 22:45:26 +0000

    Automated deployment: Mon Aug 26 22:45:26 UTC 2019 2cd242cda8eae55cffb5c7b86fb76725a838efbb

---
+++ precache-manifest.7e6613e0e99c10c9351c4a5983d3012d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "91de89b26bf6d5bde84ae53764f92ec8",
+    "url": "/WeatherApp/index.html"
+  },
+  {
+    "revision": "fb3aad1ad4b3662d3335",
+    "url": "/WeatherApp/static/css/main.c733a3f0.chunk.css"
+  },
+  {
+    "revision": "b857badcb57e8ca58909",
+    "url": "/WeatherApp/static/js/2.07523cbb.chunk.js"
+  },
+  {
+    "revision": "fb3aad1ad4b3662d3335",
+    "url": "/WeatherApp/static/js/main.e7b48ca7.chunk.js"
+  },
+  {
+    "revision": "261c1af3d55931795ea6",
+    "url": "/WeatherApp/static/js/runtime~main.3f4b0836.js"
+  }
+]);
